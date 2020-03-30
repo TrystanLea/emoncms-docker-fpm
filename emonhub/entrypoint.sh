5a3bf5dd85906c3fa7809510d4b0b9252991ca97
TrystanLea <trystan.lea@gmail.com>
2020-03-30 21:08:29 +0100

    docker emonhub.conf

--- emonhub/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Disable bluetooth device and restores UART0/ttyAMA0
-# once the device tree overlay setting is added to the /boot/config.txt the host must reboot
-
-set -e
-
-python2 ${EMONHUB_DIR}/src/emonhub.py
-
-exec "$@"
